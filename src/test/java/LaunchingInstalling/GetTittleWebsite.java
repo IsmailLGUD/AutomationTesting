8ddb7443b379c784ee7c82fdb3cd597d1aa16334
Ismail <mdismailhossain215@gmail.com>
2022-07-25 15:14:09 +0600

    Get tittle of a webpage

--- src/test/java/LaunchingInstalling/GetTittleWebsite.java
+++ src/test/java/LaunchingInstalling/GetTittleWebsite.java
@@ -3,15 +3,15 @@ package LaunchingInstalling;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class RahulShettyWebpage {
+public class GetTittleWebsite {
 
 	public static void main(String[] args) {
-
 		System.setProperty("webdriver.chrome.driver","C:/Users/MD. ISMAIL HOSSAIN/utilities/chromedriver.exe");		
 		WebDriver driver = new ChromeDriver();
 		
 		driver.get("https://rahulshettyacademy.com/");
-		
+		String Title = driver.getTitle();
+		System.out.println(Title);
 
 	}
 
