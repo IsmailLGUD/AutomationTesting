0d32dcbe3f43c4827749171639f46142eee6021c
Ismail <mdismailhossain215@gmail.com>
2022-07-25 15:07:47 +0600

    POM dependencies added and Launching Chrome browser

---
+++ src/test/java/LaunchingInstalling/TestGoogleChrome.java
@@ -0,0 +1,17 @@
+package LaunchingInstalling;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestGoogleChrome {
+
+	public static void main(String[] args) {
+
+		
+		System.setProperty("webdriver.chrome.driver","C:/Users/MD. ISMAIL HOSSAIN/utilities/chromedriver.exe");		
+		WebDriver driver = new ChromeDriver();
+		
+
+	}
+
+}
